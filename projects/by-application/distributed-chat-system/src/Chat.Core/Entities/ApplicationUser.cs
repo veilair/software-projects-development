ea1b5142d138c409d1e746c02be58fbf0846b516
Paul Veillard <36556329+veilair@users.noreply.github.com>
2022-08-28 13:53:45 -0400

    Add files via upload

---
+++ projects/by-application/distributed-chat-system/src/Chat.Core/Entities/ApplicationUser.cs
@@ -0,0 +1,9 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Identity;
+
+namespace Chat.Core.Entities
+{
+    public class ApplicationUser : IdentityUser
+    {
+    }
+}
---
+++ projects/by-application/distributed-chat-system/src/Chat.Core/Entities/ChatMessage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Chat.Core.Entities
+{
+    public class ChatMessage
+    {
+        public long Id { get; init; }
+        public string FromUserId { get; init; }
+        public string ToUserId { get; init; }
+        public string Message { get; init; }
+        public DateTime CreatedDate { get; init; }
+        public virtual ApplicationUser FromUser { get; init; }
+        public virtual ApplicationUser ToUser { get; init; }
+    }
+}
